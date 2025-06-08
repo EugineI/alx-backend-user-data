f7217d7b19a2d48ae306579504a34be60109d089
EugineI <eugineindimuli@gmail.com>
2025-06-08 17:18:48 +0300

    user and database

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+""" defines an sqlalchemy model, user"""
+Base = declarative_base()
+
+
+class User(Base):
+    """ user attributes """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
